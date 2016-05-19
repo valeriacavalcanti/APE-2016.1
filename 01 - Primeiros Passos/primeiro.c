aab274e1fd5ab4fc3d4e2da8f38295caeff965b7
Valéria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2016-05-18 22:18:28 -0300

    Primeiros passos

---
+++ 01 - Primeiros Passos/primeiro.c
@@ -0,0 +1,27 @@
+// Tudo que estiver escrito nessa linha não será compilado
+
+/*
+    Aqui também não será compilado. É o que chamamos de "comentário".
+    É um recurso muito útil para o programador !!
+    Ajuda a orientar quando estamos programando.
+*/
+
+/*
+    Diretiva do pré-processador
+    Indica que antes de compilar o código, deverá ser importada
+    a biblioteca "stdio".
+*/
+
+// standard input output
+#include <stdio.h>
+
+// Função principal. Todo código C tem que ter uma.
+int main(){ // essa função é chamada quando o código é executado
+
+    // é retorno que estamos informando para quem
+    // chamou essa função "main", ou seja, o SO.
+    return 0;
+}
+
+// as chaves indicam onde começa "{" e onde termina "}" um
+//  bloco de comandos
